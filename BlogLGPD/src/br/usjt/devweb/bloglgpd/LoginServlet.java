26d25e5d3ca95d0d717a62c3f54cc16430ca45ac
ricardon91 <ricardo_l.h.p@hotmail.com>
2020-04-04 17:16:23 -0300

    Login com Adm e usuário funcionando

--- BlogLGPD/src/br/usjt/devweb/bloglgpd/LoginServlet.java
+++ BlogLGPD/src/br/usjt/devweb/bloglgpd/LoginServlet.java
@@ -1,44 +1,42 @@
 package br.usjt.devweb.bloglgpd;
 
-import java.io.IOException;  
-import java.io.PrintWriter;  
-  
-import javax.servlet.ServletException;  
-import javax.servlet.http.HttpServlet;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
-import javax.servlet.http.HttpSession;  
-public class LoginServlet extends HttpServlet {  
-    /**
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)  
-                    throws ServletException, IOException {  
-        response.setContentType("text/html");  
-        PrintWriter out=response.getWriter();        
-          
-          
-        String name=request.getParameter("name");  
-        String password=request.getParameter("password");  
-          
-        if(password.equals("admin123")){
-        	request.getRequestDispatcher("moderacao.jsp").include(request, response);
-        	out.print("Bem Vindo, "+name);  
-        	HttpSession session=request.getSession();  
-        	session.setAttribute("name",name);        
-        }
-        
-        if (!password.equals("admin123")) {
-        	request.getRequestDispatcher("cadastro.jsp").include(request, response);
-        	out.print("Bem Vindo, "+name);  
-        	HttpSession session=request.getSession();  
-        	session.setAttribute("name",name);
-        } else {  
-            out.print("Desculpe, Usuário ou Senha Inválidos! ");  
-            request.getRequestDispatcher("login.jsp").include(request, response);  
-        }
-        out.close();  
-    }  
-}  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
+		if (password.equals("admin123")) {
+			request.getRequestDispatcher("moderacao.jsp").include(request, response);
+			out.print("Bem Vindo, " + name);
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+		} else if (password != "admin123") {
+			request.getRequestDispatcher("cadastro.jsp").include(request, response);
+			out.print("Bem Vindo, " + name);
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+		} else {
+			out.print("Desculpe, Usuário ou Senha Inválidos! ");
+			request.getRequestDispatcher("login.jsp").include(request, response);
+		}
+		out.close();
+	}
+}
